473cc8a75df95691b31b3335ff803d52fac9c8c2
dpcks9677 <dpcks9677@naver.com>
2018-05-18 23:41:30 +0900

    테스트
    jonghyok 함수에 yeah 변수와 나머지 상황 출력값 설정

--- test.py
+++ test.py
@@ -1,8 +1,14 @@
 def jongyeok(say):
     if say == "do_maple":
         print("메이뿔하자!")
-    if say == "frog":
+    elif say == "frog":
         print("아 진짜")
+    elif say == "yeah":
+        print("좋다")
+    else:
+        print("코.딩.조.아")
 
 jongyeok("do_maple")
 jongyeok("frog")
+jongyeok("yeah")
+jongyeok("suneng?")
